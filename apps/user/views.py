49abdaaff37f5b705a29088e660f85655ad237ad
Vlad0n23 <vlad.onischuk1234@gmail.com>
2024-01-11 16:08:09 +0200

    Create UserModelViewSet

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from apps.viewsets import CustomModelViewSet
 
-# Create your views here.
+from .models import User
+from . import serializers
+
+
+class UserViewSet(CustomModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserListSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return serializers.UserCreateSerializer
+        elif self.action == 'retrieve':
+            return serializers.UserDetailSerializer
+        elif self.action == 'update':
+            return serializers.UserUpdateSerializer
+        return super().get_serializer_class()
