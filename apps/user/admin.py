c85828a33e55d4ade34659b38f5ab393bb108b20
Vlad0n23 <vlad.onischuk1234@gmail.com>
2024-01-12 00:13:44 +0200

    Remove username field from UserAdmin

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -5,7 +5,7 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'is_staff', 'is_superuser')
+    list_display = ('first_name', 'last_name', 'email', 'is_staff', 'is_superuser')
     list_filter = ('is_staff', 'is_superuser')
-    search_fields = ('username', 'email')
-    ordering = ('username',)
+    search_fields = ('first_name', 'last_name', 'email')
+    ordering = ('first_name', 'last_name')
