8c197769f85e038ebf815a5f5d08eea8dba144c5
Vlad0n23 <vlad.onischuk1234@gmail.com>
2024-01-11 15:49:05 +0200

    Register User model in admin

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_superuser')
+    list_filter = ('is_staff', 'is_superuser')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
