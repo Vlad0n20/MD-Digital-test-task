c9c7529687c4a28dc08d6d0f4755d6e47c12d4bb
Vlad0n23 <vlad.onischuk1234@gmail.com>
2024-01-11 16:28:34 +0200

    Add router for UserViewSet to core urls

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from core.yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('apps.user.urls', namespace='user'), name='user'),
 ]
+
+urlpatterns += doc_urls
