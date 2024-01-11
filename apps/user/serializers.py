1e580d952a37a86a5ed41cdaeea4ca8dbfc38652
Vlad0n23 <vlad.onischuk1234@gmail.com>
2024-01-11 15:58:30 +0200

    Add new joined_duration to UserDetailSerializer

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -10,19 +10,31 @@ class UserCreateSerializer(serializers.ModelSerializer):
 
 
 class UserListSerializer(serializers.ModelSerializer):
+
+    def validate_username(self, value):
+        if 'test' in value:
+            raise serializers.ValidationError("Username cannot contain 'test'")
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Username already exists")
+        return value
+
     class Meta:
         model = User
         fields = ('id', 'username', 'email',)
 
 
 class UserDetailSerializer(serializers.ModelSerializer):
+    joined_duration = serializers.SerializerMethodField()
+
+    def get_joined_duration(self, obj) -> int:
+        return (obj.date_joined - obj.last_login).days
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'date_joined', 'is_active', 'is_staff', 'is_superuser')
+        fields = ('id', 'username', 'email', 'date_joined', 'is_active', 'is_staff', 'is_superuser', 'joined_duration')
 
 
 class UserUpdateSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'is_active', 'is_staff', 'is_superuser')
