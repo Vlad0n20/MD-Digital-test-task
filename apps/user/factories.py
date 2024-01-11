32d9269f0b2cef0e601bb1f47e098d0eac50a43b
Vlad0n23 <vlad.onischuk1234@gmail.com>
2024-01-12 00:10:45 +0200

    Add user factory

--- apps/user/factories.py
+++ apps/user/factories.py
@@ -1,15 +1,12 @@
 import factory.django
-from faker import Faker
 
 from .models import User
 
-fake = Faker()
-
 
 class UserFactory(factory.django.DjangoModelFactory):
-    first_name = fake.first_name()
-    last_name = fake.last_name()
-    email = fake.email()
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    email = factory.Faker('email')
 
     class Meta:
         model = User
